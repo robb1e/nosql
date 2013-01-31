32fe4b1c59e137e062708d579357f012132153cf
Robbie Clutton <robert.clutton@gmail.com>
2013-01-31 07:08:46 -0500

    New should not blow up

--- spec/nosql_spec.rb
+++ spec/nosql_spec.rb
@@ -24,6 +24,45 @@ describe "With ActiveRecord::Base" do
 
   context "with a null db connection" do
     before do
+      ActiveRecord::Base.connection.stub(:exec).and_raise(Nosql::Error)
+      ActiveRecord::Base.connection.stub(:exec).and_raise(Nosql::Error)
+      ActiveRecord::Base.connection.stub(:exec_query).and_raise(Nosql::Error)
+    end
+
+    it "cannot create a record" do
+      expect {
+        Post.create
+      }.to raise_error(Nosql::Error)
+    end
+
+    it "cannot use a scope" do
+      expect {
+        Post.where(id: 1).first
+      }.to raise_error(Nosql::Error)
+    end
+
+    it "cannot count" do
+      expect {
+        Post.count
+      }.to raise_error(Nosql::Error)
+    end
+
+    it "cannot update_all" do
+      expect {
+        Post.update_all "title = 'title'"
+      }.to raise_error(Nosql::Error)
+    end
+
+    it "can new an instance up" do
+      expect {
+        Post.new
+      }.to_not raise_error(Nosql::Error)
+    end
+
+  end
+
+  context "with a nosql adapter connection" do
+    before do
       Nosql::Connection.enable!
     end
 
@@ -54,5 +93,13 @@ describe "With ActiveRecord::Base" do
         Post.update_all "title = 'title'"
       }.to raise_error(Nosql::Error)
     end
+
+    it "can new an instance up" do
+      expect {
+        Post.new
+      }.to_not raise_error(Nosql::Error)
+    end
+
   end
+
 end
